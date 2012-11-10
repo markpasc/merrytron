24e32b0c1f0e52c8f1602a128d135e95789675fe
Mark Paschal <markpasc@markpasc.org>
2012-11-10 13:28:35 -0500

    Inherit these settins from the TitledListView

--- holiday/views.py
+++ holiday/views.py
@@ -59,20 +59,13 @@ class ClassicView(DetailView):
         return context
 
 
-class CanPlayView(ListView):
+class CanPlayView(TitledListView):
 
-    paginate_by = 40
-    context_object_name = 'songs'
-    template_name = 'songs.html'
+    title = 'Playable'
 
     def get_queryset(self):
         qs = Song.objects.exclude(embed='')
         return qs.select_related('album').order_by('album__artist__name', 'album__title', 'track')
-
-    def get_context_data(self, **kwargs):
-        context = super(CanPlayView, self).get_context_data(**kwargs)
-        context['title'] = 'Playable'
-        return context
 
 
 class RatedView(ListView):
