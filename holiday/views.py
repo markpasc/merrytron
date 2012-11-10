52b0713ea9b1af4f9a564f3a20dfc2bf9a2ff762
Mark Paschal <markpasc@markpasc.org>
2012-11-10 12:38:54 -0500

    Oops, don't use tabs
    And space fix and fix "paginate_by" order in class attributes.

--- holiday/views.py
+++ holiday/views.py
@@ -8,36 +8,36 @@ from holiday.models import Artist, Album, Classic, Song
 
 class TitledListView(ListView):
 
-	paginate_by = 40
-	context_object_name = 'songs'
-	template_name = 'songs.html'
-	title = None
+    context_object_name = 'songs'
+    paginate_by = 40
+    template_name = 'songs.html'
+    title = None
 
-	def get_context_data(self, **kwargs):
-		context = super(TitledListView, self).get_context_data(**kwargs)
-		context['title'] = self.title
-		return context
+    def get_context_data(self, **kwargs):
+        context = super(TitledListView, self).get_context_data(**kwargs)
+        context['title'] = self.title
+        return context
 
 
 class HomeView(ListView):
 
     queryset = Song.objects.select_related('album').order_by('-added', 'album', 'track')
-    paginate_by = 40
     context_object_name = 'songs'
+    paginate_by = 40
     template_name = 'home.html'
 
 
 class ArtistView(DetailView):
 
-	model = Artist
-	context_object_name = 'artist'
-	template_name = 'songs.html'
+    model = Artist
+    context_object_name = 'artist'
+    template_name = 'songs.html'
 
-	def get_context_data(self, **kwargs):
-		context = super(ArtistView, self).get_context_data(**kwargs)
-		query = Q(artist__slug=self.object.slug) | Q(album__artist__slug=self.object.slug)
-		context['songs'] = Song.objects.filter(query).order_by('-added', 'album', 'track')
-		return context
+    def get_context_data(self, **kwargs):
+        context = super(ArtistView, self).get_context_data(**kwargs)
+        query = Q(artist__slug=self.object.slug) | Q(album__artist__slug=self.object.slug)
+        context['songs'] = Song.objects.filter(query).order_by('-added', 'album', 'track')
+        return context
 
 
 class AlbumView(DetailView):
@@ -47,14 +47,13 @@ class AlbumView(DetailView):
     template_name = 'album.html'
 
 
-
 class ClassicView(DetailView):
 
-	model = Classic
-	context_object_name = 'classic'
-	template_name = 'songs.html'
+    model = Classic
+    context_object_name = 'classic'
+    template_name = 'songs.html'
 
-	def get_context_data(self, **kwargs):
-		context = super(ClassicView, self).get_context_data(**kwargs)
-		context['songs'] = self.object.song_set.order_by('-added', 'album', 'track')
-		return context
+    def get_context_data(self, **kwargs):
+        context = super(ClassicView, self).get_context_data(**kwargs)
+        context['songs'] = self.object.song_set.order_by('-added', 'album', 'track')
+        return context
